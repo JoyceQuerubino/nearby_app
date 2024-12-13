b47fbdfe2b57a19e58e94005770ef05098f58860
JoyceQuerubino <joycequerubino5@gmail.com>
2024-12-13 17:33:47 -0300

    firts commit

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { View } from "react-native";
+import { router } from "expo-router";
+
+import { Button } from "@/components/button";
+import { Steps } from "@/components/steps";
+import { Welcome } from "@/components/welcome";
+import {IconPlus} from "@tabler/icons-react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
+
+      <Button onPress={() => router.navigate("/home")}>
+        <Button.Title>Começar</Button.Title>
+        <Button.Icon icon={IconPlus}></Button.Icon>
+      </Button>
+    </View>
+  );
+}
---
+++ src/components/places/index.tsx
@@ -0,0 +1,41 @@
+import { useRef } from "react"
+import { Text, useWindowDimensions } from "react-native"
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet"
+
+import { s } from "./styles"
+import { Place, PlaceProps } from "../place";
+
+type Props = {
+  data: PlaceProps[]
+}
+
+export function Places({ data }: Props) {
+  const dimensions = useWindowDimensions()
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  const snapPoints = {
+    min: 278,
+    max: dimensions.height - 128, //altura de 120 + padding de place;
+  }
+
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={[snapPoints.min, snapPoints.max]}
+      handleIndicatorStyle={s.indicator}
+      backgroundStyle={s.container}
+      enableOverDrag={false} //para respeitar a mudança de tamanho
+    >
+      <BottomSheetFlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Place data={item} />}
+        contentContainerStyle={s.content}
+        ListHeaderComponent={() => (
+          <Text style={s.title}>Explore locais perto de você</Text>
+        )} //cabeçalho
+        showsVerticalScrollIndicator={false}
+      />
+    </BottomSheet>
+  )
+}
