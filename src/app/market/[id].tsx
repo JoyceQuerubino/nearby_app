b7dd5f225c43948f6eb8a1fc6d7a8ddcdd9c556c
JoyceQuerubino <joycequerubino5@gmail.com>
2024-12-14 21:58:11 -0300

    add cupom componente and screens about

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,60 +1,95 @@
-import { useEffect, useState, useRef } from "react"
-import { View, Alert, Modal, StatusBar, ScrollView } from "react-native"
-import { router, useLocalSearchParams, Redirect } from "expo-router"
+import { useEffect, useState, useRef } from "react";
+import { View, Alert, Modal, StatusBar, ScrollView } from "react-native";
+import { router, useLocalSearchParams, Redirect } from "expo-router";
+import { useCameraPermissions, CameraView } from "expo-camera";
 
-import { Button } from "@/components/button"
-import { Loading } from "@/components/loading"
+import { Button } from "@/components/button";
+import { Loading } from "@/components/loading";
 
-import { api } from "@/services/api"
-import { Cover } from "@/components/market/cover"
-import { Details, PropsDetails } from "@/components/market/details"
+import { api } from "@/services/api";
+import { Cover } from "@/components/market/cover";
+import { Details, PropsDetails } from "@/components/market/details";
+import { Coupon } from "@/components/market/coupon";
 
 type DataProps = PropsDetails & {
-  cover: string
-}
+  cover: string;
+};
 
 export default function Market() {
-  const [data, setData] = useState<DataProps>()
-  const [coupon, setCoupon] = useState<string | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
-  const [couponIsFetching, setCouponIsFetching] = useState(false)
-  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false)
+  const [data, setData] = useState<DataProps>();
+  const [coupon, setCoupon] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [couponIsFetching, setCouponIsFetching] = useState(false);
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
 
-  const params = useLocalSearchParams<{id: string}>();//pra pegar o id que vem por rota
+  const [_, requestPermission] = useCameraPermissions();
+  const params = useLocalSearchParams<{ id: string }>(); //pra pegar o id que vem por rota
 
   async function fetchMarket() {
     try {
-      const { data } = await api.get(`/markets/${params.id}`)
-      setData(data)
-      setIsLoading(false)
+      const { data } = await api.get(`/markets/${params.id}`);
+      setData(data);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       Alert.alert("Erro", "Não foi possível carregar os dados", [
         {
           text: "OK",
           onPress: () => router.back(),
         },
-      ])
+      ]);
+    }
+  }
+
+  async function handleOpenCamera() {
+    try {
+      const { granted } = await requestPermission();
+
+      if (!granted) {
+        return Alert.alert("Câmera", "Você precisa habilitar o uso da câmera");
+      }
+
+      // qrLock.current = false
+      setIsVisibleCameraModal(true);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Câmera", "Não foi possível utilizar a câmera");
     }
   }
 
   useEffect(() => {
-    fetchMarket()
-  }, [params.id])
+    fetchMarket();
+  }, [params.id]);
 
-  if(isLoading){
-    return <Loading/>
+  if (isLoading) {
+    return <Loading />;
   }
 
-  if(!data){
-    return < Redirect href={"/home"}/>
+  if (!data) {
+    return <Redirect href={"/home"} />;
   }
 
   return (
     <View style={{ flex: 1 }}>
-     <Cover uri={data.cover}/>
+      <Cover uri={data.cover} />
+      <Details data={data} />
+      {coupon && <Coupon code={coupon} />}
 
-     <Details data={data}/>
+      <View style={{ padding: 32 }}>
+        <Button onPress={handleOpenCamera}>
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <CameraView style={{ flex: 1 }} />
+
+        <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+          <Button onPress={() => setIsVisibleCameraModal(false)}>
+            <Button.Title>Voltar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
-  )
+  );
 }
